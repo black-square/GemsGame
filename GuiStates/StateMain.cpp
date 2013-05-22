4a6d0b68c8a78bf89d4a15f57ea34ab05ec51f68
dmitry.shesterkin <dfb@yandex.ru>
2013-05-22 13:50:21 +0400

    Main Menu improved

--- GuiStates/StateMain.cpp
+++ GuiStates/StateMain.cpp
@@ -8,26 +8,47 @@
 //////////////////////////////////////////////////////////////////////////
 GuiStateMain::GuiStateMain()
 {
+  AddWidget( boost::make_shared<Gui::Image>(
+    Rect( Point(0, 0), Size(755, 600) ),
+    boost::make_shared<Texture>( "./_data/backg_main.png" )
+    ));
+ 
+  const Texture::TPtr pBtnTex = boost::make_shared<Texture>( "./_data/button_01.png", 2 );
+  const Font::TPtr pBtnFont = boost::make_shared<Font>( "./_data/gm.ttf", 25);
+
+  AddWidget( boost::make_shared<Gui::Label>(
+    Point(377, 10),
+    boost::make_shared<Font>( "./_data/gm.ttf", 150), 
+    "The Game",
+    Color::make_white(),
+    Gui::Label::Center
+  ));
+
+  AddWidget( boost::make_shared<Gui::Label>(
+    Point(377, 545),
+    boost::make_shared<Font>( "./_data/gm.ttf", 50), 
+    "Dmitry Shesterkin   dfb@yandex.ru",
+    Color::make_white(),
+    Gui::Label::Center
+  ));
+
   AddWidget( boost::make_shared<Gui::Button>(
-    Rect( Point(290, 220), Size(200, 50) ),
-    boost::make_shared<Font>( "./_data/gm.ttf", 25),
-    boost::make_shared<Texture>( "./_data/button_01.png", 2 ),
+    Rect( Point(252, 220), Size(250, 50) ),
+    pBtnFont, pBtnTex,
     BIND_THIS(StartMainGameState),
     "Start Game"
   ));
 
   AddWidget( boost::make_shared<Gui::Button>(
-    Rect( Point(290, 300), Size(200, 50) ),
-    boost::make_shared<Font>( "./_data/gm.ttf", 25),
-    boost::make_shared<Texture>( "./_data/button_01.png", 2 ),
+    Rect( Point(252, 300), Size(250, 50) ),
+    pBtnFont, pBtnTex,
     BIND_THIS(StartZenGameState),
     "Zen Mode Game"
   ));
 
   AddWidget( boost::make_shared<Gui::Button>(
-    Rect( Point(290, 380), Size(200, 50) ),
-    boost::make_shared<Font>( "./_data/gm.ttf", 25),
-    boost::make_shared<Texture>( "./_data/button_01.png", 2 ),
+    Rect( Point(252, 380), Size(250, 50) ),
+    pBtnFont, pBtnTex,
     BIND_THIS(StartAutoplayState),
     "Autoplay Test"
     ));
