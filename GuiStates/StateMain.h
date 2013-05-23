7f4f693a10959c4c6a00322493a77d3eb2d0aa8c
dmitry.shesterkin <dfb@yandex.ru>
2013-05-23 14:25:09 +0400

    Particle System Test

--- GuiStates/StateMain.h
+++ GuiStates/StateMain.h
@@ -2,6 +2,7 @@
 #define GuiStateMain_h__
 
 #include "Gui/State.h"
+#include "Particles/ParticlesManager.h"
 
 class GuiStateMain: public Gui::State
 {   
@@ -14,8 +15,17 @@ public:
 private:
   void StartAutoplayState();
   void StartMainGameState();
-  void StartZenGameState(); 
-};
+  void StartZenGameState();
 
+  virtual void OnLButtonDown( Point pos );
+
+  virtual void OnUpdate( float deltaTime );
+
+  virtual void OnRender() const;
+
+private:
+  ParticlesManager m_part;
+  Texture::TPtr m_pTex;
+};
 
 #endif // GuiStateMain_h__
