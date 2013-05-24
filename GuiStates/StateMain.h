7b28198869e359dd3f474c3b6b9f0f4626548c33
dmitry.shesterkin <dfb@yandex.ru>
2013-05-24 12:54:04 +0400

    Texture blending test to explore premultiplied alpha problem on Mac

--- GuiStates/StateMain.h
+++ GuiStates/StateMain.h
@@ -2,6 +2,8 @@
 #define GuiStateMain_h__
 
 #include "Gui/State.h"
+#include "Graphics/Texture.h"
+#include "Particles/ParticlesManager.h"
 
 class GuiStateMain: public Gui::State
 {   
@@ -15,6 +17,16 @@ private:
   void StartAutoplayState();
   void StartMainGameState();
   void StartZenGameState();
+  
+private:
+  void OnRender() const;
+  void OnLButtonDown( Point pos );
+
+  virtual void OnUpdate( float deltaTime );
+
+private:
+  Texture::TPtr m_pTexTst;
+  ParticlesManager m_particles;
 };
 
 #endif // GuiStateMain_h__
